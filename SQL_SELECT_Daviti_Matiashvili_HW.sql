d50b8d053b67480b4ff7ea7808a6360b23b837fa
Daviti Matiashvili <129106403+mato08@users.noreply.github.com>
2024-11-07 20:46:15 +0400

    updated homework

--- SQL_SELECT_Daviti_Matiashvili_HW.sql
+++ SQL_SELECT_Daviti_Matiashvili_HW.sql
@@ -1,148 +1,179 @@
-/* task1
-1) select f.title from film f
-join film_category fc 
-on f.film_id =fc.film_id 
-join category c  
-on fc.category_id = c.category_id 
-where c."name" ='Animation'
-and f.release_year between 2017 and 2019
-order by f.title
+ -- task 1 
+select f.title 
+from public.film f
+join public.film_category fc on f.film_id = fc.film_id 
+join public.category c on fc.category_id = c.category_id 
+where upper(c."name") = 'ANIMATION' 
+  and f.rental_rate > 1
+  and f.release_year between 2017 and 2019
+order by f.title;
+--task 2
 
-2)with problem2 as (
-    select a.address as full_address, sum(p.amount) as revenue
-    from  address a
-    join customer c on a.address_id = c.address_id 
-    join payment p on c.customer_id = p.customer_id  
-    where p.payment_date >= '2017-03-01'
-    group by a.address
-    union all
-    select 
-        a.address2 as full_address, 
-        sum(p.amount) as revenue
-    from address a
-    join customer c on a.address_id = c.address_id 
-    join payment p on c.customer_id = p.customer_id  
-    where p.payment_date >= '2017-03-01'
-    group by a.address2 
-)
-select full_address, sum(revenue) as total_revenue
-from problem2
-group by full_address;
-3)
-SELECT a.first_name, a.last_name, COUNT(f.film_id) AS number_of_movies
-FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
-JOIN film f ON fa.film_id = f.film_id
-WHERE f.release_year >= 2015
-GROUP BY a.actor_id, a.first_name, a.last_name 
-ORDER BY number_of_movies DESC
-LIMIT 5; 
-4)
+select 
+    concat(a.address, ' ', a.address2) as addresses, 
+    sum(p.amount) as revenue
+from public.address a 
+join public.store s on a.address_id = s.address_id
+join public.inventory i on s.store_id = i.store_id 
+join public.rental r on i.inventory_id = r.inventory_id 
+join public.payment p on r.rental_id = p.rental_id 
+where extract(year from p.payment_date) >= 2017 
+  and extract(month from p.payment_date) >= 3
+group by s.store_id, addresses;
+
+/* it is not good to use string comparators on date so I used extract function to get the year and the month */
+
+
+--task 3
+
+select 
+    a.first_name, 
+    a.last_name, 
+    count(f.film_id) as number_of_movies
+from public.actor a
+join public.film_actor fa on a.actor_id = fa.actor_id
+join public.film f on fa.film_id = f.film_id
+where f.release_year >= 2015
+group by a.actor_id
+order by number_of_movies desc
+limit 5;
+
+
+--task 4
+
 select 
     f.release_year,
-    coalesce(count(case when c.name = 'Drama' then 1 end), 0) as number_of_drama_movies,
-    coalesce(count(case when c.name = 'Travel' then 1 end), 0) as number_of_travel_movies,
-    coalesce(count(case when c.name = 'Documentary' then 1 end), 0) as number_of_documentary_movies
-from film f
-join film_category fc on f.film_id = fc.film_id
-join category c on fc.category_id = c.category_id
+    count(case when upper(c.name) = 'DRAMA' then 1 end) as number_of_drama_movies,
+    count(case when upper(c.name) = 'TRAVEL' then 1 end) as number_of_travel_movies,
+    count(case when upper(c.name) = 'DOCUMENTARY' then 1 end) as number_of_documentary_movies
+from public.film f
+join public.film_category fc on f.film_id = fc.film_id
+join public.category c on fc.category_id = c.category_id
 where c.name in ('Drama', 'Travel', 'Documentary')
-group by f.release_year
+group by f.release_year 
 order by f.release_year desc;
  
+/* coalesce was not necessary because count() already treats null values as a 0 so I just deleted it*/
 
-5)
-SELECT 
-    c.customer_id,
-    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
-    f.title AS horror_film,
-    SUM(p.amount) AS total_paid
-FROM customer c
-JOIN rental r ON c.customer_id = r.customer_id
-JOIN  inventory i ON r.inventory_id = i.inventory_id
-JOIN film f ON i.film_id = f.film_id
-JOIN film_category fc ON i.film_id = fc.film_id
-JOIN category c2 ON fc.category_id = c2.category_id
-JOIN payment p ON r.rental_id = p.rental_id
-WHERE c2.name = 'Horror'
-GROUP BY c.customer_id, c.first_name, c.last_name, f.title;
+--task 5
 
-Part 2
- 1)  I wrote it with help of the chatGPT not everything but at least 50%
+select 
+    concat(c.first_name, ' ', c.last_name) as customer,
+    sum(p.amount) as amount_of_money,
+    string_agg(f.title, ', ') as list_of_horrors
+from public.customer c 
+join public.payment p on c.customer_id = p.customer_id 
+join public.rental r on p.rental_id = r.rental_id 
+join public.inventory i on r.inventory_id = i.inventory_id 
+join public.film f on i.film_id = f.film_id 
+join public.film_category fc on f.film_id = fc.film_id 
+join public.category c2 on fc.category_id = c2.category_id 
+where upper(c2."name") = 'HORROR' 
+group by c.customer_id;
+
+/* I didn't display the list of horrors so I with help of the string_agg function now I display all the horror movies 
+ * in one column
+ */
  
- WITH LastPayment AS (
-    SELECT 
-        p.staff_id,
-        MAX(p.payment_date) AS last_payment_date
-    FROM payment p
-    WHERE EXTRACT(YEAR FROM p.payment_date) = 2017
-    GROUP BY p.staff_id
-) /*This CTE computes the most recent payment date for each staff member for payments made in 2017 and I'm 
-going to use it in another select clause,for more simplicity. */
-
-SELECT 
-    s.staff_id,
-    CONCAT(s.first_name, ' ', s.last_name) AS staff_name,
-    SUM(p.amount) AS total_revenue,
-    st.store_id AS last_store
-FROM payment p
-JOIN rental r ON p.rental_id = r.rental_id
-JOIN inventory i ON r.inventory_id = i.inventory_id
-JOIN staff s ON p.staff_id = s.staff_id
-JOIN store st ON s.store_id = st.store_id
-JOIN LastPayment lp ON p.staff_id = lp.staff_id 
-                    AND p.payment_date = lp.last_payment_date
-WHERE EXTRACT(YEAR FROM p.payment_date) = 2017
-GROUP BY s.staff_id, s.first_name, s.last_name, st.store_id
-ORDER BY total_revenue DESC
-LIMIT 3; 
+ --part 2 task 1
+ 
+ select  
+    concat(s.first_name, ' ', s.last_name) as staff_name,
+    sum(p.amount) as total_revenue,
+    s.store_id as last_store
+from public.staff s
+join public.payment p on s.staff_id = p.staff_id
+join public.store s2 on s.store_id = s2.store_id 
+where extract(year from p.payment_date) = 2017
+group by s.staff_id, s.store_id 
+order by sum(p.amount) desc 
+limit 3;
+  
+  /* I don't understand the logic of this task fully so this solution can be incorrect again but I will try to explain how
+   * why i wrote this. I'm just summing the amount in payment table and just joining three tables. but the question for me 
+   * was how to know which store was the last. if employee changed the store then in staff table store_id will be changed too
+   * so because of that I decided to group them by the s.store_id and also display it.*/
+  
 
 
 
-2)
-select f.title,f.rating, count(f.film_id) as number_of_rentals 
-from film f
-join inventory i on f.film_id = i.film_id 
-join rental r on i.inventory_id =i.inventory_id 
-group by f.title ,f.film_id,f.rating 
+--part 2. task 2
+   
+
+select 
+    f.title,
+    f.rating, 
+    count(f.film_id) as number_of_rentals,
+    case
+        when f.rating = 'G' then 'all ages'
+        when f.rating = 'PG' then 'recommended age 8+'
+        when f.rating = 'PG-13' then 'recommended age 13+'
+        when f.rating = 'R' then 'recommended age 17+'
+        when f.rating = 'NC-17' then 'recommended age 18+'
+    end as recommended_age
+from public.film f
+join public.inventory i on f.film_id = i.film_id 
+join public.rental r on i.inventory_id = r.inventory_id 
+group by f.title, f.film_id, f.rating 
 order by number_of_rentals desc 
-limit 5
+limit 5;
 
-Part 3)
-variant 1-select 
-	concat(a.first_name,' ', a.last_name),
-	(2024- max(f.release_year)) as gap
-from actor a 
-join  film_actor fa  on a.actor_id =fa.actor_id 
-join film f  on fa.film_id =f.film_id 
+/* here I just added cases for all the ratings. searched the excpected ages for mpa_rating in google and add the
+ * recommended age column.
+ */
+
+-- task 3,variant 1
+select 
+    concat(a.first_name, ' ', a.last_name) as actor_name,
+    (extract(year from current_date) - max(f.release_year)) as gap
+from public.actor a 
+join public.film_actor fa on a.actor_id = fa.actor_id 
+join public.film f on fa.film_id = f.film_id 
 group by a.actor_id 
-order by gap desc 
-limit 1
-
-variant 2-SELECT 
-    CONCAT(a.first_name, ' ', a.last_name) AS actor_name,
-    f1.release_year AS film_year_1,
-    f2.release_year AS film_year_2,
-    (f2.release_year - f1.release_year) AS gap
-FROM 
-    actor a
-join film_actor fa1 ON a.actor_id = fa1.actor_id
-join film_actor fa2 ON a.actor_id = fa2.actor_id
-join film f1 ON fa1.film_id = f1.film_id
-join film f2 ON fa2.film_id = f2.film_id
-where 
-    f1.release_year < f2.release_year
-    and (f2.release_year - f1.release_year) = (
-        select max(f4.release_year - f3.release_year)
-        from film_actor fa3
-        join film f3 ON fa3.film_id = f3.film_id
-        join film_actor fa4 ON fa3.actor_id = fa4.actor_id AND fa4.actor_id = a.actor_id
-        join film f4 ON fa4.film_id = f4.film_id
-        where f3.release_year < f4.release_year
+having extract(year from current_date) - max(f.release_year) = (
+    select max(inactive_years) 
+    from (
+        select extract(year from current_date) - max(f.release_year) as inactive_years
+        from public.actor a
+        join public.film_actor fa on a.actor_id = fa.actor_id 
+        join public.film f on fa.film_id = f.film_id 
+        group by a.actor_id 
     )
-order by
-    gap desc
-limit 1
-*/
+)
+order by gap desc;
+/* here in last solution i wrote limit 1 but that was incorrect because there can be several actors with the same gap
+ * so with help of the subquery I changed limit 1 and now it shows all the actors with the maximum gap */
+
+-- task 3 variant 2
+select 
+    concat(a.first_name, ' ', a.last_name) as actor_name,
+    max(f2.release_year) - min(f1.release_year) as max_gap
+from public.actor a
+join public.film_actor fa1 on a.actor_id = fa1.actor_id
+join public.film_actor fa2 on a.actor_id = fa2.actor_id
+join public.film f1 on fa1.film_id = f1.film_id
+join public.film f2 on fa2.film_id = f2.film_id
+where f1.release_year < f2.release_year
+group by a.actor_id
+having max(f2.release_year) - min(f1.release_year) = (
+    select max(max_gap)
+    from (
+        select max(f2.release_year) - min(f1.release_year) as max_gap
+        from public.actor a
+        join public.film_actor fa1 on a.actor_id = fa1.actor_id
+        join public.film_actor fa2 on a.actor_id = fa2.actor_id
+        join public.film f1 on fa1.film_id = f1.film_id
+        join public.film f2 on fa2.film_id = f2.film_id
+        where f1.release_year < f2.release_year
+        group by a.actor_id
+    ) 
+);
+    /* i had the same mistake here. I wrote limit 1 here too.here I changed it with the subquery too and now it also 
+     * shows all the actors with the maximum gap
+     */
+  
+  
+  
+
 
 
